6392f92019ed1a35f8b9e229103c989fed3499f8
Aniruddha Murali <aniruddha.murali@gmail.com>
2019-11-02 17:47:05 -0400

    Stupid Chris

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -28,4 +28,4 @@ def condition():
         flash('Condition={} requested, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('condition.html', title='Sign In', form=form)
