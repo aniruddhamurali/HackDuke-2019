aba8bd37ec4ac1cf52ec01af2f52f508ef4a5304
ceelo777 <lochristopher9@gmail.com>
2019-11-03 08:33:34 -0500

    Changes

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -3,15 +3,19 @@ from app.forms import ConditionForm
 from flask import render_template, flash, redirect, url_for, request
 from flask_pymongo import pymongo
 from googleplaces import GooglePlaces, ranking, types, lang
+from geolocation.main import GoogleMaps
+from geolocation.distance_matrix.client import DistanceMatrixApiClient
 import json
 import requests
 import random
 
-client = pymongo.MongoClient("mongodb+srv://aliu:aliu@hackduke2019-nkevk.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://aliu:aliu@hackduke2019-nkevk.gcp.mongodb.net/test?retryWrites=true&w=majority"
+)
 db = client.inline
-hospitalList = db.HospitalCost.find()
 # Use your own API key for making api request calls
 API_KEY = 'AIzaSyBHGtFlzUzLX4251KTO3IBfen2no0Jllic'
+
 # Initialising the GooglePlaces constructor
 google_places = GooglePlaces(API_KEY)
 
@@ -19,18 +23,19 @@ google_places = GooglePlaces(API_KEY)
 # @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+    posts = [{
+        'author': {
+            'username': 'John'
         },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-
-    ]
+        'body': 'Beautiful day in Portland!'
+    }, {
+        'author': {
+            'username': 'Susan'
+        },
+        'body': 'The Avengers movie was so cool!'
+    }]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -40,32 +45,44 @@ def search():
     #     print(entry)
     form = ConditionForm()
     if form.validate_on_submit():
-        flash('Condition={}, remember_me={}'.format(
-            form.condition.data, form.remember_me.data))
+        flash('Condition={}, remember_me={}'.format(form.condition.data,
+                                                    form.remember_me.data))
         return redirect(url_for('hospitals'))
     return render_template('condition.html', title='Condition', form=form)
-    
+
+
 @app.route('/hospitals')
 def hospitals():
     my_var = request.args.get('my_var', None)
-    waitList = []
-    costList = []
-    for waitHospital in waitHospitals:
-        if my_var.lower() in waitHospital[-1]:
-            waitList.append(waitHospital)
-    for costHospital in costHospitals:
-        if my_var.lower() in costHospital[-1]:
-            waitList.append(costHospital)
-    #getNearbyHospitals(34.5289,-86.8178, 50000, hospitalList, hospitalNames)
-    getNearbyHospitals(34.5289,-86.8178, 50000, waitList, hospitalNames)
-    getNearbyHospitals(34.5289,-86.8178, 50000, costList, hospitalNames)
-    print("WAIT", waitHospitals, "COST", costHospitals)
-    return render_template('hospitals.html', title='Hospitals', wHospitals=waitHospitals, cHospitals=costHospitals)
+    hospitalList = db.HospitalCost.find()
+    hospitalNames = []
+    
+    for hospital in hospitalList:
+        hospitalNames.append(hospital['HospitalName'].lower())
+
+    getNearbyHospitals(34.5289, -86.8178, 50000, hospitalList, hospitalNames)
+    fWaitHospitals = []
+    fCostHospitals = []
+
+    for hospital in waitHospitals:
+        if (my_var.lower() in hospital[-1]):
+            fWaitHospitals.append(hospital)
+
+    for hospital in costHospitals:
+        if (my_var.lower() in hospital[-1]):
+            fCostHospitals.append(hospital)
+
+    return render_template('hospitals.html',
+                           title='Hospitals',
+                           wHospitals=fWaitHospitals,
+                           cHospitals=fCostHospitals)
+
 
 @app.route('/hospital/<hName>')
 def hospitalProfile(hospitalName):
     # get the website, phone number, addy of hospital
     return render_template('baseHospital.html', hName=hospitalName)
+
 
 @app.route('/admin')
 def hospital_admin():
@@ -73,41 +90,40 @@ def hospital_admin():
         return render_template("form.html")
     redirect(url_for('index'))
 
-@app.route('/result',methods = ['POST', 'GET'])
+
+@app.route('/result', methods=['POST', 'GET'])
 def result():
-   if request.method == 'POST':
-      result = request.form
-      treatmentArray = []
-      pippo =  request.form.to_dict()
-      for x in pippo.values():
-        treatmentArray.append(x)
-      print(session['username'])
-      '''db.Admins.update( {"name": session['username']}, {"treatments":treatmentArray})'''
-      return render_template('form.html',result = result)
+    if request.method == 'POST':
+        result = request.form
+        treatmentArray = []
+        pippo = request.form.to_dict()
+        for x in pippo.values():
+            treatmentArray.append(x)
+        print(session['username'])
+        '''db.Admins.update( {"name": session['username']}, {"treatments":treatmentArray})'''
+        return render_template('form.html', result=result)
+
 
 #
 # Supplementary functions section
 #
 
-
-hospitalNames = []
-for hospital in hospitalList:
-    hospitalNames.append(hospital['HospitalName'].lower())
-
 waitHospitals = []
 costHospitals = []
+
 
 def getNearbyHospitals(latitude, longitude, sRadius, hospitals, hospitalNames):
     # Search all hospitals within certain radius
     # (rip the rankby=ranking.DISTANCE,searchTerm="disease" didn't work)
-    query_result = google_places.nearby_search(
-        lat_lng={'lat': latitude, 'lng': longitude},
-        radius=sRadius,
-        types=[types.TYPE_HOSPITAL]
-    )
+    query_result = google_places.nearby_search(lat_lng={
+        'lat': latitude,
+        'lng': longitude
+    },
+                                               radius=sRadius,
+                                               types=[types.TYPE_HOSPITAL])
     # If any attributions related with search results print them
-    if query_result.has_attributions:
-        print(query_result.html_attributions)
+    '''if query_result.has_attributions:
+        print(query_result.html_attributions)'''
 
     # Iterate over the search results
     for place in query_result.places:
@@ -115,9 +131,10 @@ def getNearbyHospitals(latitude, longitude, sRadius, hospitals, hospitalNames):
         # url variable to store google maps' url
         url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
         #print(place.geo_location['lat'], place.geo_location['lng'])
-        r = requests.get(url + 'origins=' + str(latitude) + ',' + str(longitude) +
-                         '&destinations=' + str(place.geo_location['lat']) + ',' + str(place.geo_location['lng']) +
-                         '&key=' + API_KEY)
+        r = requests.get(url + 'origins=' + str(latitude) + ',' +
+                         str(longitude) + '&destinations=' +
+                         str(place.geo_location['lat']) + ',' +
+                         str(place.geo_location['lng']) + '&key=' + API_KEY)
         x = r.json()  # json method of response object
 
         # from the API response, get the drive time and address
@@ -138,13 +155,13 @@ def getNearbyHospitals(latitude, longitude, sRadius, hospitals, hospitalNames):
                     costHospital = hospital
                     costTuple = (
                         name,  # name
-                        time,                  # time
+                        time,  # time
                         address,
                         float(place.geo_location['lat']),  # latitude
                         float(place.geo_location['lng']),  # longitude
-                        distance,              # distance
+                        distance,  # distance
                         costHospital['AvgOutOfPocketCost'],  # cost
-                        costHospital['tags']    # filters
+                        costHospital['tags']  # filters
                     )
                     costHospitals.append(costTuple)
                     break
@@ -153,9 +170,9 @@ def getNearbyHospitals(latitude, longitude, sRadius, hospitals, hospitalNames):
         # we still register it, insert based on waiting time
         else:
             ref = {
-                0: "urgent_care", 
-                1: "blood_loss", 
-                2: "mental_health", 
+                0: "urgent_care",
+                1: "blood_loss",
+                2: "mental_health",
                 3: "infection",
                 4: "pediatrics",
                 5: "poison",
@@ -163,19 +180,13 @@ def getNearbyHospitals(latitude, longitude, sRadius, hospitals, hospitalNames):
                 7: "sickness",
                 8: "sports_injuries"
             }
-            randNum = random.randint(3, 9)
+            randNum = random.randint(3, 8)
             tagsToAdd = []
             for i in range(randNum):
                 newRand = random.randint(0, 8)
-                tagsToAdd.append(ref[newRand])
-            
-            waitTuple = (
-                name,
-                time,
-                address,
-                float(place.geo_location['lat']),
-                float(place.geo_location['lng']),
-                distance,
-                tagsToAdd
-            )
+                if ref[newRand] not in tagsToAdd:
+                    tagsToAdd.append(ref[newRand])
+
+            waitTuple = (name, time, address, float(place.geo_location['lat']),
+                         float(place.geo_location['lng']), distance, tagsToAdd)
             waitHospitals.append(waitTuple)
