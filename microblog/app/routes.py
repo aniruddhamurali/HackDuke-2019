a34c793070fd37b75ccd9c88c4a8deb019922315
ceelo777 <lochristopher9@gmail.com>
2019-11-02 22:24:44 -0400

    Connected to mongo database

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template
 from app import app
 from app.forms import ConditionForm
 from flask import render_template, flash, redirect, url_for
+from flask_pymongo import pymongo
 
+client = pymongo.MongoClient("mongodb+srv://aliu:aliu@hackduke2019-nkevk.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test_database
 
 @app.route('/')
 @app.route('/index')
@@ -21,8 +23,11 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/')
 @app.route('/condition', methods=['GET', 'POST'])
 def condition():
+    for entry in client.test_database.find():
+        print(entry)
     form = ConditionForm()
     if form.validate_on_submit():
         flash('Condition={}, remember_me={}'.format(
