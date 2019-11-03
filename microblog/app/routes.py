3da53a71292028e0ac9d6b30fd9be548f41e83cd
Zhziong <40403161+Zhziong@users.noreply.github.com>
2019-11-03 08:39:59 -0500

    Login Stuff

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -17,8 +17,11 @@ API_KEY = 'AIzaSyBHGtFlzUzLX4251KTO3IBfen2no0Jllic'
 google_places = GooglePlaces(API_KEY)
 globalUser = "Test"
 
+
+'''
+Home page
+'''
 @app.route('/')
-# @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -33,7 +36,35 @@ def index():
 
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+<<<<<<< HEAD
 
+# Login
+@app.route('/login', methods=['POST','GET'])
+def login():
+    form = LoginForm()
+    users = db.Admins
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('/'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = db.Admins
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+<<<<<<< HEAD
 @app.route('/login', methods=['POST'])
 def login():
     users = db.Admins
@@ -67,16 +98,27 @@ def register():
             session['username'] = request.form['username']
             return redirect(url_for('index'))
 
+=======
+>>>>>>> 10d71d0297f245d215d0e0687ecdea65d688c5c5
         return 'That username already exists!'
 
     return render_template('register.html')
 
+<<<<<<< HEAD
 @app.route('/signout')
 def signout():
     if 'username' in session:
         session['username'] = None
     redirect(url_for('index'))
 
+=======
+=======
+>>>>>>> ef7b86e4b1480efa316b4be7c88045958ad0f395
+
+'''
+Search function
+'''
+>>>>>>> 10d71d0297f245d215d0e0687ecdea65d688c5c5
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     items = db.HospitalCost
@@ -93,15 +135,36 @@ def search():
 @app.route('/hospitals')
 def hospitals():
     my_var = request.args.get('my_var', None)
-    getNearbyHospitals(34.5289,-86.8178, 50000, hospitalList, hospitalNames)
+    waitList = []
+    costList = []
+    for waitHospital in waitHospitals:
+        if my_var.lower() in waitHospital[-1] and waitHospital not in waitHospitals:
+            waitList.append(waitHospital)
+    for costHospital in costHospitals and costHospital not in costHospitals:
+        if my_var.lower() in costHospital[-1]:
+            waitList.append(costHospital)
+    #getNearbyHospitals(34.5289,-86.8178, 50000, hospitalList, hospitalNames)
+    getNearbyHospitals(34.5289,-86.8178, 50000, waitList, hospitalNames)
+    getNearbyHospitals(34.5289,-86.8178, 50000, costList, hospitalNames)
     print("WAIT", waitHospitals, "COST", costHospitals)
     return render_template('hospitals.html', title='Hospitals', wHospitals=waitHospitals, cHospitals=costHospitals)
 
+
+'''
+Personalized hospital page
+'''
 @app.route('/hospital/<hName>')
 def hospitalProfile(hospitalName):
     # get the website, phone number, addy of hospital
     return render_template('baseHospital.html', hName=hospitalName)
 
+<<<<<<< HEAD
+
+'''
+#########################################################################################################################################
+#########################################################################################################################################
+'''
+=======
 @app.route('/admin')
 def hospital_admin():
     if 'username' in session:
@@ -120,11 +183,11 @@ def result():
       print(db.Admins.find({"name": globalUser}))
       db.Admins.update( {"name": globalUser}, {"treatments":treatmentArray})
       return render_template('form.html',result = result)
+>>>>>>> ef7b86e4b1480efa316b4be7c88045958ad0f395
 
 #
 # Supplementary functions section
 #
-
 
 hospitalNames = []
 for hospital in hospitalList:
@@ -203,8 +266,14 @@ def getNearbyHospitals(latitude, longitude, sRadius, hospitals, hospitalNames):
             tagsToAdd = []
             for i in range(randNum):
                 newRand = random.randint(0, 8)
+<<<<<<< HEAD
                 tagsToAdd.append(ref[newRand])
 
+=======
+                if ref[newRand] not in tagsToAdd:
+                    tagsToAdd.append(ref[newRand])
+            
+>>>>>>> 10d71d0297f245d215d0e0687ecdea65d688c5c5
             waitTuple = (
                 name,
                 time,
